488dfb335f244426278469f3ef2a1f05f160ceca
bangjaehun <qkdwogns98@gmail.com>
2020-05-14 09:38:10 +0900

    brute force 3

--- brute-force/index.js
+++ brute-force/index.js
@@ -173,3 +173,43 @@ function shortestPath(paths, visited, currentLength) {
 
   return result;
 }
+
+// Clock Sync
+const SWITCH = 10;
+const CLOCKS = 16;
+const LINKED = [[]]; // !![i][j]: Switch 'i' Linked Clock 'j'
+
+// Are Clocks point to '12'?
+function areAligned(clocks) { }
+
+// Push switch
+function push(clocks, swtch) {
+  for (let clock = 0; clock < CLOCKS; clock += 1) {
+    if (LINKED[swtch][clock] === 'y') {
+      clocks[clock] += 3;
+
+      // 15 == 3 (about over 12)
+      if (clocks[clock] === 15) clocks[clock] = 3;
+    }
+  }
+}
+
+// main
+function solve(clocks, swtch) {
+  if (swtch === SWITCH) return areAligned(clocks) ? 0 : INF; // INF: 123456789
+
+  let result = INF;
+
+  // count 1: up '3' hour
+  // count 4 == count 0, max count = 4
+  for (let count = 0; count < 4; count += 1) {
+
+    // recursive, update
+    result = Math.min(result, count + solve(clocks, swtch + 1));
+
+    push(clocks, swtch);
+  }
+
+  return result;
+}
+

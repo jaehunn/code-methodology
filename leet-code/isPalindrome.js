05df245daa47744808ee2d659d886b7932a3c82d
bangjaehun <qkdwogns98@gmail.com>
2020-06-07 13:22:37 +0900

    palindrome, roman integer

---
+++ leet-code/isPalindrome.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+
+function isPalindrome(x) {
+  if (x < 0) return false; // negative
+  if (x < 10) return true; // units 1
+  if (x % 10 === 0) return false; // deca(= 10^n)
+
+  let rev = 0;
+  while (rev < x) {
+    rev *= 10; // move
+    rev += x % 10; // set
+
+    x = Math.trunc(x / 10); // next
+
+    console.log(rev);
+  }
+
+  return rev === x || Math.trunc(rev / 10) === x;
+}

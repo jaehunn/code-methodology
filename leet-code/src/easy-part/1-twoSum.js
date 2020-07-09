73c225a6e1b9ab1efb5af1da1cf480d18636a6af
bangjaehun <qkdwogns98@gmail.com>
2020-07-09 18:35:30 +0900

    twoSum

---
+++ leet-code/src/easy-part/1-twoSum.js
@@ -0,0 +1,27 @@
+// @see https://leetcode.com/problems/two-sum/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+/*
+    nums 에 포함된 두 정수는 target 에 만족한다. 두 정수의 Indices 를 구하라. 
+*/
+function twoSum(nums, target) {
+  // Key 가 정수고, Value 가 인덱스인 해쉬를 생성한다.
+  const m = {};
+
+  for (let i = 0; i < nums.length; i += 1) {
+    // 보수를 구하고 캐싱한다.
+    let c = target - nums[i];
+    if (c in m) return [m[c], i];
+
+    // 캐싱에 실패했다면, 메모한다.
+    m[nums[i]] = i;
+  }
+
+  return null;
+}
+
+console.log(twoSum([1, 2, 3, 4, 5], 8));

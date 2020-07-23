23f77bd455c793912153539e1b04dcfbfd529ce2
bangjaehun <qkdwogns98@gmail.com>
2020-07-23 13:54:59 +0900

    sqrt

--- leet-code/src/easy-part/jul/4th/17-sqrt(x).js
+++ leet-code/src/easy-part/jul/4th/17-sqrt(x).js
@@ -3,4 +3,24 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function (x) {};
+
+// ex) x = 5
+function mySqrt(x) {
+  let l = 0;
+  let r = x; // 5
+
+  // 0 < 5
+  // 3 < 5
+  // 3 == 3 break;
+  while (l < r) {
+    const m = Math.floor((l + r) / 2); // 2, 4
+
+    if (m * m === x) return m;
+    if (x < m * m) r = m - 1;
+    // r = 3
+    else l = m + 1; // l = 3
+  }
+
+  // 5 < 9, 5 = 2.xxx -> return 2;
+  return x < r * r ? r - 1 : r;
+}

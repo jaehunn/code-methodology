60d54d40a4e7912801ae56dee69db4ba1160be82
bangjaehun <qkdwogns98@gmail.com>
2020-07-28 13:59:35 +0900

    maximum depth orderTraversal

---
+++ leet-code/src/easy-part/jul/5th/24-Ordertraversal.js
@@ -0,0 +1,39 @@
+// @see https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function (root) {
+  if (root === null) return [];
+
+  let r = [];
+  let q = [root]; // enqueue
+
+  while (q.length) {
+    let s = q.length;
+    let c = [];
+
+    for (let i = 0; i < s; i += 1) {
+      let h = q.shift(); // dequeue
+
+      c.push(h.val);
+
+      // enqueue
+      // *Null Checking
+      if (h.left !== null) q.push(h.left);
+      if (h.right !== null) q.push(h.right);
+    }
+
+    r.unshift(c);
+  }
+
+  return r;
+};

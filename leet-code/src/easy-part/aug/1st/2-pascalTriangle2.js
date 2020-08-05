4aa1e282ecb60250f143d0fa140515a16c7a17e7
bangjaehun <qkdwogns98@gmail.com>
2020-08-05 13:09:55 +0900

    pascal advance

---
+++ leet-code/src/easy-part/aug/1st/2-pascalTriangle2.js
@@ -0,0 +1,57 @@
+// @see https://leetcode.com/problems/pascals-triangle-ii/
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function (rowIndex) {
+  let r = [1];
+
+  for (let i = 1; i <= rowIndex; i += 1) {
+    for (let j = i; j > 0; j -= 1) {
+      if (j === i) r[j] = 1;
+      else r[j] = r[j - 1] + r[j];
+    }
+  }
+
+  return r;
+};
+
+/* 
+    rowIndex = 3
+    0: [1]
+    1: [1, 1]
+    2: [1, 2, 1]
+    *3: [1, 3 ,3, 1]
+    4: [1, 4, 6, 4, 1]
+*/
+
+var _getRow = function (rowIndex) {
+  let r = [1];
+
+  for (let i = 0; i < rowIndex; i += 1) {
+    r.unshift(1);
+
+    for (let j = 0; j <= i; j += 1) {
+      r[j] = (r[j + 1] || 0) + (r[j + 2] || 0);
+    }
+
+    r.pop();
+    r.unshift(1);
+  }
+
+  return r;
+};
+
+// rowIndex = 1
+// [(1), 1]
+// [1, 0] -> [1] -> [1, 1]
+
+// rowIndex = 2
+// [(1), 1, 1]
+// [2, 1, 0] -> [2, 1] -> [1, 2, 1]
+
+// rowIndex = 3
+// [(1), 1, 2, 1]
+// [3, 3, 1, 0] -> [3, 3, 1] -> [1, 3, 3, 1]
+
+console.log(_getRow(3));

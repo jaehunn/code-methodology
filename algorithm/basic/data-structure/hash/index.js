6b930f7f6309f2d3245fae33f340b2dc3c39e8b7
bangjaehun <qkdwogns98@gmail.com>
2020-08-17 13:27:22 +0900

    HashTable

---
+++ algorithm/basic/data-structure/hash/index.js
@@ -0,0 +1,72 @@
+import LinkedList from "../linkedList";
+
+const defaultHashTableSize = 32;
+
+export default class HashTable {
+  constructor(hashTableSize = defaultHashTableSize) {
+    this.buckets = Array(hashTableSize)
+      .fill(null)
+      .map(() => new LinkedList());
+
+    this.keys = {};
+  }
+
+  hash(key) {
+    const hash = Array.from(key).reduce(
+      (hashAccumulator, keySymbol) => hashAccumulator + keySymbol.charCodeAt(0),
+      0
+    );
+
+    // normalization
+    return hash % this.buckets.length;
+  }
+
+  set(key, value) {
+    const keyHash = keyHash(key);
+
+    this.keys[key] = keyHash; // keys['string'] = keyHash
+
+    const bucketLinkedList = this.buckets[keyHash]; // find list
+    const node = bucketLinkedList.find({
+      callback: (nodeValue) => nodeValue.key === key,
+    });
+
+    if (!node) {
+      bucketLinkedList.append({ key, value });
+    } else {
+      node.value.value = value;
+    }
+  }
+
+  delete(key) {
+    const keyHash = this.hash(key);
+    delete this.keys[key];
+
+    const bucketLinkedList = this.buckets[keyHash];
+    const node = bucketLinkedList.find({
+      callback: (nodeValue) => nodeValue.key === key,
+    });
+
+    if (!node) return bucketLinkedList.delete(node.value);
+
+    return null;
+  }
+
+  get(key) {
+    const keyHash = this.hash(key);
+    const bucketLinkedList = this.buckets[keyHash];
+    const node = bucketLinkedList.find({
+      callback: (nodeValue) => nodeValue.key === key,
+    });
+
+    return node ? node.value.value : undefined;
+  }
+
+  has(key) {
+    return Object.hasOwnProperty.call(this.keys, key);
+  }
+
+  getKeys() {
+    return Object.keys(this.keys);
+  }
+}

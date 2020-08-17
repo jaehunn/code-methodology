4a7b051084ca23af47e37b0238a026ac8ee38271
bangjaehun <qkdwogns98@gmail.com>
2020-08-17 13:01:18 +0900

    Stack

---
+++ algorithm/basic/data-structure/stack/index.js
@@ -0,0 +1,33 @@
+export default class Stack {
+  constructor() {
+    this.linkedList = new LinkdedList();
+  }
+
+  isEmpty() {
+    return !this.linkedList.head;
+  }
+
+  peek() {
+    if (this.isEmpty()) return null;
+
+    return this.linkedList.head.value;
+  }
+
+  push(value) {
+    this.linkedList.prepend(value);
+  }
+
+  pop() {
+    const removedHead = this.linkedList.deleteHead();
+
+    return removedHead ? removedHead.value : null;
+  }
+
+  toArray() {
+    return this.linkedList.toArray().map((node) => node.value);
+  }
+
+  toString(callback) {
+    return this.linkedList.toString(callback); // delegation
+  }
+}

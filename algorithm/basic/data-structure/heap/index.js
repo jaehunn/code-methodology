e96713ef988cfa872337825a3862b5d9f7bf755b
bangjaehun <qkdwogns98@gmail.com>
2020-08-18 13:10:35 +0900

    heap

--- algorithm/basic/data-structure/heap/index.js
+++ algorithm/basic/data-structure/heap/index.js
@@ -1,6 +1,6 @@
 import Comparator from "../../../utils";
 
-export default class Heap {
+export class Heap {
   constructor(comparatorFunction) {
     if (new.target === Heap)
       throw new Error("Cannot construct Heap instance directly");
@@ -58,7 +58,7 @@ export default class Heap {
     return this.heapContainer[0];
   }
 
-  poll() {
+  extract() {
     if (this.heapContainer.length === 0) return null;
 
     if (this.heapContainer.length === 1) return this.heapContainer.pop();
@@ -78,7 +78,7 @@ export default class Heap {
   }
 
   find(item, comparator = this.compare) {
-    const foundItemIndices = [];
+    const indices = [];
 
     for (
       let itemIndex = 0;
@@ -86,13 +86,102 @@ export default class Heap {
       itemIndex += 1
     ) {
       if (comparator.equal(item, this.heapContainer[itemIndex]))
-        foundItemIndices.push(itemIndex);
+        indices.push(itemIndex);
     }
 
-    return foundItemIndices;
+    return indices;
+  }
+
+  // abstract result of order
+  // Max-heap: first >= second
+  // Min-heap: first <= second
+  correctOrder(first, second) {
+    throw new Error(` You have to implement heap pair comparision method
+    for ${first} and ${second} values.`);
+  }
+
+  // parent
+  heapifyUp(startIndex) {
+    let index = startIndex || this.heapContainer.length - 1;
+
+    while (
+      this.hasParent(index) &&
+      !this.correctOrder(this.parent(index), this.heapContainer[index])
+    ) {
+      this.swap(index, this.getParentIndex(index)); // swap
+
+      index = this.getParentIndex(index); // move
+    }
+  }
+
+  // child, parent
+  heapifyDown(startIndex) {
+    let index = startIndex || 0;
+    let nextIndex = null;
+
+    while (this.hasLeftChild(index)) {
+      nextIndex =
+        this.hasRightChild(index) &&
+        this.correctOrder(this.rightChild(index), this.leftChild(index))
+          ? this.getRightChildIndex(index)
+          : this.getLeftChildIndex(index);
+
+      if (
+        this.correctOrder(
+          this.heapContainer[index],
+          this.heapContainer[nextIndex]
+        )
+      )
+        break;
+
+      this.swap(index, nextIndex); // swap
+
+      index = nextIndex; // move
+    }
   }
 
   remove(item, comparator = this.compare) {
-    // WIP
+    const numberOfRemoves = this.find(item, comparator).length;
+
+    for (let i = 0; i < numberOfRemoves; i += 1) {
+      const index = this.find(item, comparator).pop();
+
+      if (index === this.heapContainer.length - 1) {
+        this.heapContainer.pop();
+      } else {
+        this.heapContainer[index] = this.heapContainer.pop(); // swap
+
+        const parentItem = this.parent(index);
+
+        // if no parent, then heapifyDown(), otherwise heapifyUp()
+        // 1. leaf -> up()
+        // 2. no leaf -> down()
+        if (
+          this.hasLeftChild(index) &&
+          (!parentItem ||
+            this.correctOrder(parentItem, this.heapContainer[index]))
+        ) {
+          this.heapifyDown(index);
+        } else {
+          this.heapifyUp(index);
+        }
+      }
+    }
+
+    return this;
+  }
+}
+
+export class MaxHeap extends Heap {
+  // overriding
+  correctOrder(first, second) {
+    return this.compare.greaterThanOrEqual(first, second);
+  }
+}
+
+export class MinHeap extends Heap {
+  // overriding
+  correctOrder(first, second) {
+    return this.compare.lessThanOrEqual(first, second);
   }
 }

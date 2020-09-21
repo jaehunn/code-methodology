3b1ee7580c6e8b9fddf9464ef19a7af8c9fbdaa9
jaehunn <qkdwogns98@gmail.com>
2020-09-21 19:00:38 +0900

    promise...

--- lang/async/index.js
+++ lang/async/index.js
@@ -627,10 +627,280 @@ clear();
 
 // 6. pattern
 {
+  // 1. Promise.all([ ]) - gate pattern
+  {
+    var rq1 = request("http://some.url.1/");
+    var rq2 = request("http://some.url.2/");
+
+    Promise.all([rq1, rq2])
+      .then(function (v) {
+        // v = [v1,v2]
+
+        return request("http://some.url.3/?v=" + v.join(","));
+      })
+      .then(function (v) {
+        log(v);
+      });
+  }
+
+  // if rq1 or rq2 error -> no fulfilled, always attach errror callack
+
+  // 2. Promise.race([ ]) - latch pattern
+  {
+    var rq1 = request("http://some.url.1/");
+    var rq2 = request("http://some.url.2/");
+
+    Promise.race([rq1, rq2])
+      .then(function (v) {
+        // v is not array
+        return request("http://some.url.3/?v=" + v);
+      })
+      .then(function (v) {
+        // v
+      });
+  }
+
+  // Promise.race([ ]) time-out pattern
+  {
+    // time-out function -> after 3s rejected
+    Promise.race([foo(), timeoutPromise(3000)]).then(
+      function () {
+        // foo
+      },
+      function (e) {
+        // foo disapear or rejected
+        // time over 3s
+      }
+    );
+  }
+
+  // finally
+  {
+    var f = Promise.resolve(42);
+
+    // resolved -> always call(cleanup)
+    f.then(function () {})
+      .finally(function () {})
+      .then(function () {})
+      .finally(function () {});
+  }
+
+  // polyfill
+  {
+    if (!Promise.observe) {
+      Promise.observe = function (p, cb) {
+        p.then(
+          function (v) {
+            Promise.resolve(v).then(cb);
+          },
+          function (e) {
+            Promise.resolve(e).then(cb);
+          }
+        );
+
+        return p;
+      };
+    }
+
+    // use
+    Promise.race([
+      Promise.observe(foo(), function cleanup(v) {
+        // foo cleanup (even though foo timeover)
+      }),
+      timeoutPromise(3000),
+    ]);
+  }
+
+  // 1. none() -> every reject
+  // 2. any() -> some fulfilled
+  // 3. first() -> latch
+  // 4. last() -> last fulfilled
+
+  // first code
+  {
+    if (!Promise.first) {
+      Promise.first = function (ps) {
+        return new Promise(function (res, rej) {
+          ps.forEach(function (p) {
+            Promise.resolve(p).then(res);
+          });
+        });
+      };
+    }
+  }
+
+  // simultaneous iteration
+  {
+    if (!Promise.map) {
+      Promise.map = function (vals, cb) {
+        return Promise.all(
+          vals.map(function (val) {
+            return new Promise(function (res) {
+              cb(val, res);
+            });
+          })
+        );
+      };
+    }
+
+    // use
+    var p1 = Promise.resolve(21);
+    var p2 = Promise.resolve(42);
+    var p3 = Promise.reject("error");
+
+    Promise.map([p1, p2, p3], function (p, done) {
+      Promise.resolve(p).then(function (v) {
+        done(v * 2);
+      }, done);
+    }).then(function (vals) {
+      vals; // 42 84 'errors'
+    });
+  }
 }
 
 // 7. api
 {
+  // new Promise()
+  {
+    // 프라미스 콜백 = IIFE
+    // IIFE 인자 = 귀결 처리 콜백(성공, 실패)
+    var f = new Promise(function (res, rej) {
+      // res()
+      // rej()
+    });
+
+    // reject() = 프라미스를 버린다.
+    // resolve() => 즉시값은 즉시값, 프라미스/데너블 재귀적으로 풀어서 최종값으로
+  }
+
+  // Promise.reject()
+  {
+    var f = new Promise(function (res, rej) {
+      reject("Error");
+    });
+
+    var g = Promise.reject("Error");
+    // f 와 g 는 같다.
+  }
+
+  // Promise.resolve()
+  {
+    // Promise.resolve(), Promise.reject() 는 이미 이루어진 프라미스를 생성하는 용도로 쓰인다.
+    // 데너블을 재귀적으로 풀어 최종값을 프라미스로 한다.
+    // 프라미스면 아무일도 하지않는다. (오버헤드 없음)
+    // thenable
+    var f = {
+      then: function (cb) {
+        cb(42);
+      },
+    };
+
+    var g = {
+      then: function (cb, errCb) {
+        errCb("Error");
+      },
+    };
+
+    var _f = Promise.resolve(f); // 이룸 프라미스
+    var _g = Promise.resolve(g); // 버림 프라미스
+  }
+
+  // then(), catch()
+  // 프라미스 인스턴스에는 then() catch() 메서드가 들어있다.
+  // then() 의 콜백으로 이룸/버림 처리기를 등록할 수 있다. 두 처리기 중 하나만 비동기적으로 호출된다.
+  // then() 에 처리기를 등록하지 않으면, 기본 처리기로 대체된다.
+  // catch() 메서드는 버림 콜백만 받는다. 이룸 콜백은 기본 처리기가 된다. then(null, )
+  // then() catch() 메서드도 새 프라미스를 만든다. 따라서 연쇄 흐름 제어를 표현할 수 있다.
+  // 메서드 콜백에서 에러가 발생하면, 프라미스는 버려진다.
+  // 콜백의 반환값이 즉시값이면 이룸값으로 지정하고 특정 프라미스나 데너블이면 값을 풀어 귀결값이된다.
+
+  // Promise.all() Promise.race()
+  // Promise.all() 은 프라미스 배열을 받아 모두 이루어지는 지를 확인합니다. 하나라도 버려지면, 버려진다.
+  // 이루어지면, 배열을 반환한다. Gate 패턴이다.
+
+  // Promise.race() 는 프라미스 배열을 받아 최초로 귀결된 프라미스가 반환 프라미스의 귀결값이된다. latch 패턴이다.
 }
 
 // 8. limit
+{
+  // 프라미스 설계상 연쇄에서 에러가 나면 조용히 묻힌다.
+  // 연쇄에서 에러가 나면 어딘가에서 잡히기전까지 하위로 계속 내려간다.
+  {
+    var f = foo(42).then().then(); // f 의 프라미스는 마지막 then() 의 반환 프라미스다.
+
+    f.catch(); // catch 를 등록하면 각단계에서 처리못하는에러를 언제 에러가나도 반드시 받아 처리된다.
+    // 하지만 이미 처리된 버림같은 경우 전혀 통지받지 못하는 상황이 벌어질 수 있다.
+    // try catch 에도 기본적으로 존재하는 한계로 잡혀도 묻힐 가능성은 있다.
+    // 프라미스 연쇄에서 중간 단게를 참조하는 레퍼런스가 없어 정확하게 에러를 솎아내기란 불가능하다.
+  }
+
+  // 프라미스는 정의상 하나의 이룸, 하나의 버림을 가진다.
+  // 로직이 복잡해지면 곤란해지는 상황이온다. 해결하기 위해서 단계마다 배열을 감싸고 푸는 것은 불편하다.
+  // 값 분할하기: 2개 이상의 프라미스로 분해해보자.
+  {
+    function getY(x) {
+      return new Promise(function (res, rej) {
+        setTimeout(function () {
+          res(3 * x - 1);
+        }, 100);
+      });
+    }
+
+    function foo(bar, baz) {
+      var x = bar * baz;
+
+      return getY(x).then(function (y) {
+        return [x, y];
+      });
+    }
+
+    foo(10, 20).then(function (msg) {
+      var x = msg[0];
+      var y = msg[1];
+
+      log(x, y); // 200 599
+    });
+  }
+  // x 와 y 를 배열로 감싸 프라미스 하나로 전달할 필요가 없도록 foo 가 반환하는 것을 다시 조정하자.
+
+  {
+    function getY(x) {
+      return new Promise(function (res, rej) {
+        setTimeout(function () {
+          res(3 * x - 1);
+        }, 100);
+      });
+    }
+
+    function foo(bar, baz) {
+      var x = bar * baz;
+
+      return [Promise.resolve(x), getY(x)];
+    }
+
+    Promise.all(foo(10, 20)).then(function (msgs) {
+      var x = msgs[0];
+      var y = msgs[1];
+
+      log(x, y); // 200 59
+    });
+
+    // Promise.all 패턴이 더 나은 코드라고 할 수 있다.
+    // x 와 y 값을 별도 함수로 분리할 수 있고 호출부에서 프라미스를 알아서 조정할 수 있다.
+    // foo 안에서 로직을 추상화하는 것보다 낫다.
+  }
+
+  // 인자 스프레드하기
+  {
+    // var x 나 var y 는 불필요한 오버헤드다.
+    function spread(fn) {
+      return Function.apply.bind(fn, null);
+    }
+
+    Promise.all(foo(10, 20)).then(
+      spread(function (x, y) {
+        log(x, y);
+      })
+    );
+  }
+}
--- lang/async/test.js
+++ lang/async/test.js
@@ -1,25 +1,21 @@
 const log = console.log;
 
-function f(cb) {
-  setTimeout(function () {
-    try {
-      var x = g(); // g.e() is async
-
-      cb(null, x);
-    } catch (e) {
-      cb(e); // never
-    }
-  }, 100);
+function spread(fn) {
+  return Function.apply.bind(fn, null);
 }
 
-f(function (e, v) {
-  if (e) console.error("e" + e);
-  // error
-  else console.log("v" + v);
-});
+log(
+  spread(function (x, y) {
+    log(x, y);
+  }).call(null, [1, 2])
+);
 
-// g 가 동기, 비에러면 x 에 값이 할당 -> v 출력
-// g 가 동기, 에러면 catch -> e 출력
+function foo(x) {
+  return x;
+}
 
-// g 가 비동기면 에러면, x 에 값 미할당 -> undefined
-// g 가 비동기 비에러면,
+function bar(fn) {
+  return Function.call.bind(fn, null);
+}
+
+log(bar(foo)("test"));
